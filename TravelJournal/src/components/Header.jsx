84eebd8b8cf2e06d27608cfff2181b9e47f88648
Bishwa-b <93580657+Bishwa-b@users.noreply.github.com>
2025-01-31 18:13:52 +0530

    Add files via upload

---
+++ TravelJournal/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="header-container">
+        <img src="src/assets/planet-earth.png" alt="Globe logo" />
+        <h1>my travel journal.</h1>
+      </div>
+    </header>
+  );
+}

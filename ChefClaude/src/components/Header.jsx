b740978a8916c51a0dd786d80445e0897cdc8bf0
Bishwa-b <bishwaranjanbarik672@gmail.com>
2025-02-11 14:01:39 +0530

    Updates Images

--- ChefClaude/src/components/Header.jsx
+++ ChefClaude/src/components/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 export default function Header(props) {
   const modeIcon = props.mode
-    ? 'src/assets/light-mode.png'
-    : 'src/assets/night-mode.png';
+    ? '/light-mode.png'
+    : '/night-mode.png';
 
   const altMsg = props.mode ? 'light Mode' : 'Dark Mode';
 
   return (
     <header className={props.mode ? 'header-dark' :'header-light'}>
-      <img src="src/assets/chef-logo.png" alt="Chef Logo" />
+      <img src="/chef-logo.png" alt="Chef Logo" />
       <h1>Chef Claude</h1>
       <button className="bgButton" onClick={props.toggle}>
         <img src={modeIcon} alt={altMsg} className="modes" />
